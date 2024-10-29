d8c018789b8ae600e5227bd40a9f6ed1c84a4e0b
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-29 19:56:19 +0300

    Fix exception where entire buffer writes to stream

--- src/Http/HttpListenerResponseSender.cs
+++ src/Http/HttpListenerResponseSender.cs
@@ -12,19 +12,14 @@ public class HttpListenerResponseSender : IHttpResponseSender
 
 		var buffer = ArrayPool<byte>.Shared.Rent(body.Length);
 
-		try
-		{
-			int byteCount = Encoding.UTF8.GetBytes(body, 0, body.Length, buffer, 0);
+		int byteCount = Encoding.UTF8.GetBytes(body, 0, body.Length, buffer, 0);
 
-			response.ContentLength64 = byteCount;
+		response.ContentLength64 = byteCount;
 
-			using var outputStream = response.OutputStream;
-			await outputStream.WriteAsync(buffer, cancellationToken);
-			await outputStream.FlushAsync(cancellationToken);
-		}
-		finally
-		{
-			ArrayPool<byte>.Shared.Return(buffer);
-		}
+		using var outputStream = response.OutputStream;
+		await outputStream.WriteAsync(buffer, offset: 0, count: byteCount, cancellationToken);
+		await outputStream.FlushAsync(cancellationToken);
+
+		ArrayPool<byte>.Shared.Return(buffer);
 	}
 }
