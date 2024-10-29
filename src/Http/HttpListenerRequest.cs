13c2aab0f75a9d66abe7516cb33d4556890321c5
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-29 19:28:53 +0300

    Rename HttpRequest -> HttpListenerRequest

---
+++ src/Http/HttpListenerRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Redplcs.GtfoOfMyServer.Http;
+
+public class HttpListenerRequest(HttpListenerConnection connection) : IHttpRequest
+{
+	public IHttpConnection Connection => connection;
+}
--- src/Http/HttpRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Net;
-
-namespace Redplcs.GtfoOfMyServer.Http;
-
-public class HttpRequest(HttpListenerRequest request) : IHttpRequest
-{
-	public IPEndPoint RemoteEndPoint { get; } = request.RemoteEndPoint;
-}
